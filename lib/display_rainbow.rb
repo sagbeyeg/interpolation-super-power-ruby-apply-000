7ac365729789dcc10f2ecfebc5eff438ee26771d
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-08-14 20:34:55 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,11 @@
 # Write your #display_rainbow method here
+
+def 
+display_rainbow
+  puts "R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"
+end
+
+print display_rainbow (['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+
+#display_rainbow must accept an argument, an array of colors. The tests call #display_rainbow with the following invocation: display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+#display_rainbow should print out the colors of the rainbow in the following format: "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet" by reading from the array passed in as an argument. (For this lab it is OK to hardcode the uppercase letters.)
