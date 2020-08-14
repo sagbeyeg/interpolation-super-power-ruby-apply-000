00a8cfa6972937f6718000982ddcf74a3af237e2
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-08-14 20:36:52 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 # Write your #display_rainbow method here
 
 def 
-display_rainbow
+display_rainbow (colors)
   puts "R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"
 end
 
